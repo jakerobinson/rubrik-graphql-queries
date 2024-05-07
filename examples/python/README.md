3f03fc27ae80af780d8831b459aa4ffe41494873
jakerobinson <jake.robinson@rubrik.com>
2024-05-07 11:08:03 -0400

    python example

---
+++ examples/python/README.md
@@ -0,0 +1,27 @@
+# Generic Python example class to run GraphQL queries against Rubrik Security Cloud
+
+## Authentication
+Authenticating to the Rubrik Security Cloud API requires a Rubrik Security Cloud Service Account. The service account is OAuth2 client credentials. Basically, this means you will have a clientId and secret that are used to retrieve an access token. The access token is legitimate for 12 hours.
+
+Instantiate RSC client and connect:
+```
+import rubrik
+rsc = rubrik.Rsc_Client("INSTANCE_NAME.my.rubrik.com","CLIENT ID GOES HERE","CLIENT SECRET GOES HERE")
+```
+
+## Query Invocation
+The `invoke_query` method will accept a query string and variables. The query can be read from a file (recommended), or supplied as a string.
+
+Getting a list of SLA Domains:
+```
+result = rsc.invoke_query("{slaDomains { nodes { name id }}}")
+```
+
+Getting a VM using the `getVmByName.gql` file from the examples and supplying query variables:
+```
+file_contents = open("../../getVmByName.gql")
+query = file_contents.read()
+variables = {'name'= "sh1-fs-01"}
+result = rsc.invoke_query(query,variables)
+```
+
---
+++ examples/python/rubrik.py
@@ -0,0 +1,75 @@
+import requests
+
+class Rsc_Client:
+    def __init__(self, fqdn, client_id, client_secret):
+        self.rsc_instance = fqdn
+        self.token_url = f"https://{self.rsc_instance}/api/client_token"
+        self.gql_endpoint = f"https://{self.rsc_instance}/api/graphql"
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.token = None
+        self.get_token()
+
+    def get_token(self):
+        payload = {
+            'grant_type': 'client_credentials',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
+        }
+        response = requests.post(self.token_url, data=payload)
+        if response.status_code == 200:
+            self.token = response.json().get('access_token')
+        else:
+            raise Exception('Failed to get token: {}'.format(response.text))
+
+    def invoke_query(self, query, variables=None):
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.token),
+            'Content-Type': 'application/json'
+        }
+        payload = {'query': query}
+        if variables:
+            payload['variables'] = variables
+        response = requests.post(self.gql_endpoint, json=payload, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception('GraphQL query failed: {}'.format(response.text))
+
+    def get_sla_by_name(self, name):
+        query = """
+        query getSlaByName($name: String) {
+            slaDomains(filter: { field: NAME, text: $name }) {
+                nodes {
+                    name
+                    id
+                }
+            }
+        }
+        """
+        variables = {'name': name}
+        return self.invoke_query(query, variables)
+
+    def assign_sla(self, slaDomainAssignType, objectIds, slaOptionalId=None, shouldApplyToExistingSnapshots=None, shouldApplyToNonPolicySnapshots=None, existingSnapshotRetention=None):
+        valid_types = ['noAssignment', 'doNotProtect', 'protectWithSlaId']
+        if slaDomainAssignType not in valid_types:
+            raise ValueError(f"Invalid slaDomainAssignType: {slaDomainAssignType}. Must be one of {valid_types}.")
+
+        query = """
+        mutation assignSla($input: AssignSlaInput!) {
+            assignSla(input: $input) {
+                success
+            }
+        }
+        """
+        variables = {
+            'input': {
+                'slaDomainAssignType': slaDomainAssignType,
+                'objectIds': objectIds,
+                'slaOptionalId': slaOptionalId,
+                'shouldApplyToExistingSnapshots': shouldApplyToExistingSnapshots,
+                'shouldApplyToNonPolicySnapshots': shouldApplyToNonPolicySnapshots,
+                'existingSnapshotRetention': existingSnapshotRetention
+            }
+        }
+        return self.invoke_query(query, variables)
